825cd5ce91d97b04b1ddba1bcdfd71cf79fef0e2
Mika Pi <mika@mika.global>
2022-05-14 13:53:18 -0700

    Add support for floats and doubles

--- Source/QofL/Public/QofL/get_prop.h
+++ Source/QofL/Public/QofL/get_prop.h
@@ -1,17 +1,57 @@
 // Copyright (c) 2021 Mika Pi
 
 #pragma once
+#include "check_ret.h"
+#include "log.h"
 #include <CoreMinimal.h>
 #include <type_traits>
 
 template <typename T>
-auto getProp(UObject *obj, const FName &propName) -> T *
+auto getProp(UObject *obj, const FName &propName)
 {
-  if constexpr (std::is_base_of<UObject, T>::value)
+  auto genericProperty = obj->GetClass()->FindPropertyByName(propName);
+  if constexpr (std::is_base_of_v<UObject, T>)
   {
-    auto property = CastField<FObjectProperty>(obj->GetClass()->FindPropertyByName(propName));
-    if (!property)
-      return nullptr;
+    CHECK_RET(genericProperty, static_cast<T *>(nullptr));
+    auto property = CastField<FObjectProperty>(genericProperty);
+    CHECK_RET(property, static_cast<T *>(nullptr));
     return Cast<T>(property->GetPropertyValue_InContainer(obj, 0));
   }
+  else if constexpr (std::is_same_v<float, T>)
+  {
+    CHECK_RET(genericProperty, std::optional<float>{});
+    auto property = CastField<FFloatProperty>(genericProperty);
+    CHECK_RET(property, std::optional<float>{});
+    return std::optional<float>{property->GetPropertyValue_InContainer(obj, 0)};
+  }
+  else if constexpr (std::is_same_v<double, T>)
+  {
+    CHECK_RET(genericProperty, std::optional<double>{});
+    auto property = CastField<FDoubleProperty>(genericProperty);
+    CHECK_RET(property, std::optional<double>{});
+    return std::optional<double>{property->GetPropertyValue_InContainer(obj, 0)};
+  }
+}
+
+template <typename T>
+auto setProp(UObject *obj, const FName &propName, T value)
+{
+  auto genericProperty = obj->GetClass()->FindPropertyByName(propName);
+  if (!genericProperty)
+  {
+    LOG("Property not found:", propName.ToString());
+    return;
+  }
+  auto property = [&]() {
+    if constexpr (std::is_same_v<float, T>)
+      return CastField<FFloatProperty>(genericProperty);
+    else if constexpr (std::is_same_v<double, T>)
+      return CastField<FDoubleProperty>(genericProperty);
+  }();
+  if (!property)
+  {
+    LOG("Wrong type:", propName.ToString());
+    return;
+  }
+  property->SetPropertyValue_InContainer(obj, value, 0);
 }
