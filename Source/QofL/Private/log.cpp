4e13fd50d5caac0a7cbc84ce921c925bc63d3ce8
Mika Pi <mika@mika.global>
2024-01-28 16:14:23 -0800

    Put strings in quotes

--- Source/QofL/Private/log.cpp
+++ Source/QofL/Private/log.cpp
@@ -23,38 +23,66 @@ namespace Internal
   }
 } // namespace Internal
 
-auto operator<<(std::ostream &strm, const FString &val) -> std::ostream &
+auto toStrm(std::ostream &strm, const FString &val) -> std::ostream &
 {
-  strm << TCHAR_TO_UTF8(*val);
+  strm << "\"" << TCHAR_TO_UTF8(*val) << "\" ";
   return strm;
 }
 
-auto operator<<(std::ostream &strm, const FText &val) -> std::ostream &
+auto toStrm(std::ostream &strm, const FText &val) -> std::ostream &
 {
-  strm << TCHAR_TO_UTF8(*val.ToString());
+  strm << "\"" << TCHAR_TO_UTF8(*val.ToString()) << "\" ";
   return strm;
 }
 
-auto operator<<(std::ostream &strm, const FVector &val) -> std::ostream &
+auto toStrm(std::ostream &strm, const FVector &val) -> std::ostream &
 {
-  strm << "(" << val.X << ", " << val.Y << ", " << val.Z << ")";
+  strm << "(" << val.X << ", " << val.Y << ", " << val.Z << ") ";
   return strm;
 }
 
-auto operator<<(std::ostream &strm, const FRotator &val) -> std::ostream &
+auto toStrm(std::ostream &strm, const FRotator &val) -> std::ostream &
 {
-  strm << "(" << val.Pitch << ", " << val.Yaw << ", " << val.Roll << ")";
+  strm << "(" << val.Pitch << ", " << val.Yaw << ", " << val.Roll << ") ";
   return strm;
 }
 
-auto QOFL_API operator<<(std::ostream &strm, const TCHAR *v) -> std::ostream &
+auto toStrm(std::ostream &strm, const TCHAR *v) -> std::ostream &
 {
-  strm << TCHAR_TO_UTF8(v);
+  strm << "\"" << TCHAR_TO_UTF8(v) << "\" ";
   return strm;
 }
 
-auto QOFL_API operator<<(std::ostream &strm, const FName &v) -> std::ostream &
+auto toStrm(std::ostream &strm, const FName &v) -> std::ostream &
 {
-  strm << TCHAR_TO_UTF8(*v.ToString());
+  strm << "\"" << TCHAR_TO_UTF8(*v.ToString()) << "\" ";
+  return strm;
+}
+
+auto toStrm(std::ostream &strm, const std::string &v) -> std::ostream &
+{
+  strm << "\"" << v << "\" ";
+  return strm;
+}
+
+auto toStrm(std::ostream &strm, const char *v) -> std::ostream &
+{
+  if (!v)
+  {
+    strm << "null ";
+    return strm;
+  }
+  if (strlen(v) == 0)
+  {
+    strm << "\"\" ";
+    return strm;
+  }
+  strm << v << " ";
+  return strm;
+}
+
+auto toStrm(std::ostream &strm, const std::string_view &v) -> std::ostream &
+{
+  strm << "\"" << v << "\" ";
   return strm;
 }
--- Source/QofL/Public/QofL/log.h
+++ Source/QofL/Public/QofL/log.h
@@ -5,12 +5,21 @@
 #include <Math/Vector.h>
 #include <sstream>
 
-auto QOFL_API operator<<(std::ostream &strm, const FString &) -> std::ostream &;
-auto QOFL_API operator<<(std::ostream &strm, const FText &) -> std::ostream &;
-auto QOFL_API operator<<(std::ostream &strm, const FVector &) -> std::ostream &;
-auto QOFL_API operator<<(std::ostream &strm, const FRotator &) -> std::ostream &;
-auto QOFL_API operator<<(std::ostream &strm, const TCHAR *) -> std::ostream &;
-auto QOFL_API operator<<(std::ostream &strm, const FName &) -> std::ostream &;
+auto QOFL_API toStrm(std::ostream &strm, const FString &) -> std::ostream &;
+auto QOFL_API toStrm(std::ostream &strm, const FText &) -> std::ostream &;
+auto QOFL_API toStrm(std::ostream &strm, const FVector &) -> std::ostream &;
+auto QOFL_API toStrm(std::ostream &strm, const FRotator &) -> std::ostream &;
+auto QOFL_API toStrm(std::ostream &strm, const TCHAR *) -> std::ostream &;
+auto QOFL_API toStrm(std::ostream &strm, const FName &) -> std::ostream &;
+auto QOFL_API toStrm(std::ostream &strm, const std::string &) -> std::ostream &;
+auto QOFL_API toStrm(std::ostream &strm, const char *) -> std::ostream &;
+auto QOFL_API toStrm(std::ostream &strm, const std::string_view &) -> std::ostream &;
+
+auto toStrm(std::ostream &strm, auto v) -> std::ostream &
+{
+  strm << std::move(v) << " ";
+  return strm;
+}
 
 namespace Internal
 {
@@ -21,7 +30,7 @@ namespace Internal
   void log(const char *fileName, int line, Args &&...args)
   {
     std::ostringstream strm;
-    ((strm << std::forward<Args>(args) << " "), ...);
+    (toStrm(strm, std::forward<Args>(args)), ...);
     strm << fileName << ":" << line;
     dumpLog(strm.str().c_str());
   }
@@ -29,7 +38,7 @@ namespace Internal
   void logErr(const char *fileName, int line, Args &&...args)
   {
     std::ostringstream strm;
-    ((strm << std::forward<Args>(args) << " "), ...);
+    (toStrm(strm, std::forward<Args>(args)), ...);
     strm << fileName << ":" << line;
     dumpLogErr(strm.str().c_str());
   }
@@ -38,7 +47,7 @@ namespace Internal
   void logInfo(const char *fileName, int line, Args &&...args)
   {
     std::ostringstream strm;
-    ((strm << std::forward<Args>(args) << " "), ...);
+    (toStrm(strm, std::forward<Args>(args)), ...);
     strm << fileName << ":" << line;
     dumpLogInfo(strm.str().c_str());
   }
@@ -47,7 +56,7 @@ namespace Internal
   void shortLog(Args &&...args)
   {
     std::ostringstream strm;
-    ((strm << std::forward<Args>(args) << " "), ...);
+    (toStrm(strm, std::forward<Args>(args)), ...);
     dumpLog(strm.str().c_str());
   }
 } // namespace Internal
